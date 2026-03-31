58afafca699d424736696d08558e3aec7644d145
youb nader <youb.nader@gmail.com>
2026-03-31 01:33:07 +0100

    Add section separators and footer to Home component layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,22 @@ import About from "./components/aboutSection";
 import Agenda from "./components/agendaSection";
 import Club from "./components/clubSection";
 import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import SectionSeparator from "./components/sectionSeparator";
 
 export default function Home() {
   return (
     <main className="flex flex-col w-full">
       <Navbar />
       <Hero />
+      <SectionSeparator className="max-w-6xl mx-auto px-4" />
       <About />
+      <SectionSeparator className="max-w-6xl mx-auto px-4" />
       <Agenda />
+      <SectionSeparator className="max-w-6xl mx-auto px-4" />
       <Club />
+      <SectionSeparator className="max-w-6xl mx-auto px-4" />
+      <Footer />
     </main>
   );
 }
